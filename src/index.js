bea1ce33ac5f37c619886d8915768762c524646a
scyclow <spike716@gmail.com>
2016-03-19 16:56:09 -0400

    changed colors. made more interactive

--- src/index.js
+++ src/index.js
@@ -4,41 +4,58 @@ import { render } from 'react-dom';
 import _ from 'lodash';
 import colors from './colors';
 
-// require('../styles/application.scss');
+const BASE_COLOR = '#FF0000'
 
 class App extends React.Component {
   state = {
     dots: [],
-    fill: colors.randHex()
+    fill: '#a7fd90'
   }
 
   componentDidMount() {
     const fps = 25;
 
-    setInterval(() => {
-      const dots = this.makeDots()
-      this.setState({ dots })
-    }, 1000/fps);
+    setInterval(this.makeDots.bind(this), 1000/fps);
 
-    setInterval(() => {
-      this.setState({ fill: colors.randHex()})
-    }, 2000)
+    document.onmousemove = (e) => {
+      this.setState({ fill: this.getColor(e) })
+    };
+
+    document.onclick = (e) => {
+      document.body.style['background-color'] = colors.randHex();
+    }
+  }
+
+  getColor(e) {
+    const hueChange    = Math.floor((e.clientX / window.innerWidth) * 360);
+    const maxSatChange = 0.66;
+    const satChange    = - (maxSatChange) + ((e.clientY / window.innerHeight) * maxSatChange);
+
+    return colors.applyToHex(BASE_COLOR, { h: hueChange, s: satChange });
   }
 
   makeDots() {
     let dots = [];
-    const amount = _.random(700,900);
-    const {fill} = this.state;
+    const baseW   = 1280;
+    const baseR   = 47
+    const adjust  = window.innerWidth / baseW;
+    const minDots = 700 * adjust;
+    const maxDots = 900 * adjust;
+    const maxR    = baseR - ((baseR * (1 - adjust)) / 2);
+
+    const amount   = _.random(minDots, maxDots);
+    const { fill } = this.state;
+
     _.times(amount, () => {
       dots.push({
-        cx: _.random(0, 1240),
-        cy: _.random(0, 800),
-        r: _.random(0, 40),
+        cx: _.random(0, window.innerWidth),
+        cy: _.random(0, window.innerHeight),
+        r:  _.random(0, maxR),
         fill
       });
     });
 
-    return dots;
+    this.setState({ dots })
   }
 
   render() {
